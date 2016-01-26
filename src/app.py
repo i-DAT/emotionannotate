e470546b5e750e4bc28a60d6d391fd856c084ea6
Chris Hunt <mail@thisisthechris.co.uk>
2016-01-26 19:48:01 +0000

    Moves loading process to start of flask app

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ from Classifier import parallelClassifier, initFeatureProcessors
 app = Flask(__name__)
 app.debug = True
 
-#lexicon_feat, embed_feat = initFeatureProcessors()
+lexicon_feat, embed_feat = initFeatureProcessors()
 
 @app.route('/')
 def index():
@@ -16,11 +16,11 @@ def animation():
 
 @app.route('/all')
 def all():
-    return render_template('all.html')    
+    return render_template('all.html')
 
 @app.route('/learning', methods=['POST'])
 def learning():
-    lexicon_feat, embed_feat = initFeatureProcessors()
+    #lexicon_feat, embed_feat = initFeatureProcessors()
     data = json.loads(request.data)
     # try 'lucky @USERID ! good luck @USERID & see you soon :) @USERID @USERID'
     result = parallelClassifier([data], lexicon_feat, embed_feat)
